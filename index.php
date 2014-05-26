b17066bf8ed597ea64edf3bcf4bc7ae11a8fe7f5
masantos22 <masantos22@gmail.com>
2014-05-26 12:38:45 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 	require '/usr/local/Slim/Slim/Slim.php';
 	\Slim\Slim::registerAutoloader();
-   
+   	
+   	echo "Marcelo";
 	$app = new \Slim\Slim();
 	$app->get('/hello/:name',function($name){
 		echo "Hello, $name";
