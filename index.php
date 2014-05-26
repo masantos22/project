b2bb570a5068399739924d0f4d94701f9007b3eb
marcelo <masantos22@gmail.com>
2014-05-26 13:01:45 -0700

    Echo test in another branch

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 	require '/usr/local/Slim/Slim/Slim.php';
 	\Slim\Slim::registerAutoloader();
-   	
+	echo "Test!";   	
 	$app = new \Slim\Slim();
 	$app->get('/hello/:name',function($name){
 		echo "Hello, $name";
