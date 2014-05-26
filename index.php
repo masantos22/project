7b72e41b321ca67e7a683ffdfac452692c80ed65
marcelo <masantos22@gmail.com>
2014-05-26 12:41:24 -0700

    Echo removed

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 	require '/usr/local/Slim/Slim/Slim.php';
 	\Slim\Slim::registerAutoloader();
    	
-   	echo "Marcelo";
 	$app = new \Slim\Slim();
 	$app->get('/hello/:name',function($name){
 		echo "Hello, $name";
